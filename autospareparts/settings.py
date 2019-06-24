a9c10756e9c6ada938a9b7ac72e3e394278e26e8
LR <larp0319@gmail.com>
2019-06-24 14:58:37 -0700

    Automated tests fixed

--- autospareparts/settings.py
+++ autospareparts/settings.py
@@ -138,5 +138,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
--- stock/tests.py
+++ stock/tests.py
@@ -1,11 +1,46 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Product, Shipment
+from .models import Brand, Type, Category, AutoCategory, Product, Shipment
 import json
 
 
 class ProductTests(APITestCase):
+    def setUp(self):
+        brand = Brand.objects.create(name='Tests Brand')
+        type = Type.objects.create(name='Tests Type')
+        category = Category.objects.create(name='Tests Category')
+        autoCategory = AutoCategory.objects.create(name='Tests AutoCategory')
+
+        product = Product.objects.create(code = "00001",
+                                            model = "00001",
+                                            brand = brand,
+                                            type = type,
+                                            category = category,
+                                            autoCategory = autoCategory,
+                                            quantity=1)
+
+        self.product = {
+            "code":"00001",
+            "model":"00001",
+            "brand":"1",
+            "type":"1",
+            "category":"1",
+            "autoCategory":"1",
+            "quantity":"1"
+        }
+
+        self.product_edit = {
+            "code":"00002",
+            "model":"00002",
+            "brand":"1",
+            "type":"1",
+            "category":"1",
+            "autoCategory":"1",
+            "quantity":"2"
+        }
+
+
     def test_list_product(self):
         """
         Ensure we can list existing products.
@@ -20,7 +55,7 @@ class ProductTests(APITestCase):
         Ensure we can create a new product.
         """
         url = reverse('stock')
-        data = {"code":"00006","model":"00006","brand":"0","type":"0","category":"0","autoCategory":"0","quantity":"1"}
+        data = self.product
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -29,13 +64,41 @@ class ProductTests(APITestCase):
         """
         Ensure we can edit a existing product.
         """
-        url = reverse('stock_detail', kwargs={'pk': '1'})
-        data = {"code":"000010","model":"000010","brand":"0","type":"0","category":"0","autoCategory":"0","quantity":"10"}
+        product = Product.objects.get(code='00001')
+        print("product: " + str(product.id))
+
+        url = reverse('stock_detail', kwargs={'pk': product.id})
+        print("url: " + str(url))
+        data = data = self.product_edit
         response = self.client.put(url, json.dumps(data), content_type='application/json')
+        print("reponse.data: " + str(response.data))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class ShipmentTests(APITestCase):
+    def setUp(self):
+        brand = Brand.objects.create(name='Tests Brand')
+        type = Type.objects.create(name='Tests Type')
+        category = Category.objects.create(name='Tests Category')
+        autoCategory = AutoCategory.objects.create(name='Tests AutoCategory')
+
+        product = Product.objects.create(code = "00001",
+                                            model = "00001",
+                                            brand = brand,
+                                            type = type,
+                                            category = category,
+                                            autoCategory = autoCategory,
+                                            quantity=1)
+
+        self.shipment = {
+            "product": product.id,
+            "quantity": "1",
+            "name": "Test ship",
+            "company": "Company test",
+            "address": "asdfasdfasdf"
+        }
+
+
     def test_list_shipment(self):
         """
         Ensure we can list existing products.
@@ -50,6 +113,6 @@ class ShipmentTests(APITestCase):
         Ensure we can create a new shipment.
         """
         url = reverse('shipment')
-        data = {"id": 1, "product": "00001", "quantity": "10", "name": "testing", "company": "testing", "address": "testing"}
+        data = self.shipment
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
